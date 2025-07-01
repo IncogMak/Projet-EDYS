c59bf8b5272207e7d00f5ab7711d751cf2774ad5
Yann Elvis <yannelvis83@gmail.com>
2025-07-01 16:44:49 +0200

    modification sur pageDossier

--- PHP/PageDossier.php
+++ PHP/PageDossier.php
@@ -6,7 +6,9 @@ site web*/
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>MySugesstion
+
+  </title>
 </head>
 
 <body>
