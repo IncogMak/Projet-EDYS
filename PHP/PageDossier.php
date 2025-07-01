3840ced33116e1a49d7c637356077db4c7bb395c
Yann Elvis <yannelvis83@gmail.com>
2025-07-01 16:41:58 +0200

    styles.css devient index.css

--- PHP/PageDossier.php
+++ PHP/PageDossier.php
@@ -1,2 +1,16 @@
 /* ici reprensente la page pour permettre au etudiant ou a l'administration d'envoyer des fichier sur notre
 site web*/
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
